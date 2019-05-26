852b689aa46370857a6e441a025b94e34452c2f6
Halen <911452497@qq.com>
2019-05-26 12:10:07 +0800

    init

---
+++ src/com/halen/step1/P_003.java
@@ -0,0 +1,36 @@
+package com.halen.step1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P_003 {
+
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < length; i++) {
+            if (!set.add(numbers[i])) {
+                duplication[0] = numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean duplicate_2(int numbers[],int length,int [] duplication) {
+        int m = -1;
+        for (int i = 0; i < length; i++) {
+            while (i != numbers[i]) {
+                m = numbers[i];
+                if (m == numbers[m]) {
+                    duplication[0] = m;
+                    return true;
+                } else {
+                    numbers[i] = numbers[m];
+                    numbers[m] = m;
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/com/halen/step1/P_004.java
@@ -0,0 +1,22 @@
+package com.halen.step1;
+
+public class P_004 {
+
+    public boolean Find(int target, int [][] array) {
+        if(array.length==0 || array[0].length==0)
+            return false;
+        int row = 0;
+        int column = array[0].length-1;
+        while (row < array.length && column >= 0) {
+            int num = array[row][column];
+            if (num == target)
+                return true;
+            else if (num > target)
+                column--;
+            else
+                row++;
+        }
+        return false;
+    }
+
+}

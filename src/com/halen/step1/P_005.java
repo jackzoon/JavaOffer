6ded7053c32b11477a9d8b75355262b5ae9147ac
Halen <911452497@qq.com>
2019-05-26 13:17:34 +0800

    替换空格

--- src/com/halen/step1/P_005.java
+++ src/com/halen/step1/P_005.java
@@ -1,7 +1,20 @@
 package com.halen.step1;
 
+/**
+ * 替换空格
+ */
 public class P_005 {
 
-
+    public String replaceSpace(StringBuffer str) {
+        StringBuffer res = new StringBuffer();
+        int len = str.length() -1;
+        for (int i = len; i >= 0; i--) {
+            if(str.charAt(i) == ' ')
+                res.append("02%");
+            else
+                res.append(str.charAt(i));
+        }
+        return res.reverse().toString();
+    }
 
 }

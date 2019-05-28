f626d38748732cf2971a1689ebedc60d06667ff0
Halen <911452497@qq.com>
2019-05-28 23:07:18 +0800

    用两个栈实现队列

---
+++ src/com/halen/step1/P_008.java
@@ -0,0 +1,25 @@
+package com.halen.step1;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈实现队列
+ */
+public class P_008 {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+
+}
